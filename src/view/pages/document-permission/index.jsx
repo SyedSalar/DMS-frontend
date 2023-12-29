42ff289b583f3e63017ad941410756437c80a3db
hamza-ahad-cygnis <hamza.ahad@cygnismedia.com>
2023-12-30 04:42:17 +0500

    permission integration

--- src/view/pages/document-permission/index.jsx
+++ src/view/pages/document-permission/index.jsx
@@ -26,35 +26,38 @@ import { Checkbox } from "antd";
 
 const columns = [
   {
-    title: "MDR ID",
+    title: "ID",
     dataIndex: "id",
     key: "id",
   },
-
+  {
+    title: "MDR ID",
+    dataIndex: "masterDocumentId",
+    key: "masterDocumentId",
+  },
 
   {
     title: "User ID",
-    dataIndex: "uid",
-    key: "uid",
+    dataIndex: "userId",
+    key: "userId",
   },
   {
     title: "Reviewer",
-    dataIndex: "Reviewer",
-    key: "Reviewer",
+    dataIndex: "allowReview",
+    key: "allowReview",
   },
 
   {
     title: "Approver",
-    dataIndex: "Approver",
-    key: "Approver",
+    dataIndex: "allowApprove",
+    key: "allowApprove",
   },
 
   {
     title: "Creator",
-    dataIndex: "Creator",
-    key: "Creator",
+    dataIndex: "allowCreate",
+    key: "allowCreate",
   },
-
 ];
 const permissionsOptions = [
   {
@@ -71,8 +74,9 @@ const permissionsOptions = [
   },
 ];
 
-export default function Projects() {
-  const [projectModalVisible, setProjectModalVisible] = useState(false);
+export default function DocumentPermissions() {
+  const [DocumentPermissionModalVisible, setDocumentPermissionModalVisible] =
+    useState(false);
   const [mdrOptions, setMdrData] = useState([]);
   const [userOptions, setUserData] = useState([]);
   const [permissionUser, setpermissionUser] = useState("");
@@ -83,12 +87,47 @@ export default function Projects() {
 
   const [user, setUser] = useState(JSON.parse(localStorage?.getItem("user")));
   const [data, setData] = useState([]);
-  const [projectOptions, setProjects] = useState([]);
+  const [DocumentPermissionOptions, setDocumentPermissions] = useState([]);
+  const [checked, setChecked] = useState([]);
 
-  const [projectId, setProjectId] = useState("");
+  const [DocumentPermissionId, setDocumentPermissionId] = useState("");
 
-  const projectModalShow = () => {
-    setProjectModalVisible(true);
+  const DocumentPermissionModalShow = () => {
+    setDocumentPermissionModalVisible(true);
+  };
+  const addPermission = async () => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8083/api/documents/permissions",
+        {
+          masterDocumentId: mdr,
+          userId: permissionUser,
+          companyId: user?.user?.companyId,
+          createDocument: checked?.includes("Creator"),
+          reviewDocument: checked?.includes("Reviewer"),
+          approveDocument: checked?.includes("Approver"),
+        },
+        {
+          headers: {
+            Authorization: user?.accessToken,
+            // Add other headers if needed
+          },
+        }
+      );
+      // Handle the response as needed
+      console.log(response);
+      message.success(response?.data?.message);
+      setMDR("");
+      setpermissionUser("");
+      setChecked([]);
+      fetchData();
+      setDocumentPermissionModalVisible(false);
+    } catch (error) {
+      // Handle errors
+      if (error?.message == "Request failed with status code 403") {
+        message.error("Permission Denied to create document on this MDR");
+      }
+    }
   };
   const fetchMDR = async () => {
     try {
@@ -106,18 +145,19 @@ export default function Projects() {
         options.push({
           value: item?.id,
           label: item?.title,
-          projectId: item?.projectId,
+          DocumentPermissionId: item?.DocumentPermissionId,
           departmentId: item?.departmentId,
         });
       }
 
-      setMdrData(options); // Assuming the response.data is an array of projects
+      setMdrData(options); // Assuming the response.data is an array of DocumentPermissions
     } catch (error) {
       console.error("Error fetching departments:", error?.message);
     }
   };
   const onChange = (checkedValues) => {
     console.log("checked = ", checkedValues);
+    setChecked(checkedValues);
   };
   const fetchUsers = async () => {
     try {
@@ -139,14 +179,14 @@ export default function Projects() {
         });
       }
 
-      setUserData(options); // Assuming the response.data is an array of projects
+      setUserData(options); // Assuming the response.data is an array of DocumentPermissions
     } catch (error) {
       console.error("Error fetching departments:", error?.message);
     }
   };
-  const projectModalCancel = () => {
+  const DocumentPermissionModalCancel = () => {
     setMDR("");
-    setProjectModalVisible(false);
+    setDocumentPermissionModalVisible(false);
   };
 
   const permissionModalShow = () => {
@@ -155,11 +195,28 @@ export default function Projects() {
   const permissionModalCancel = () => {
     setPermissionModalVisible(false);
   };
-  const addProject = async () => {};
+  const addDocumentPermission = async () => {};
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:8083/api/documents/permissions?companyId=${user?.user?.companyId}`,
+        {
+          headers: {
+            Authorization: user?.accessToken,
+            // Add other headers if needed
+          },
+        }
+      );
+      setData(response.data); // Assuming the response.data is an array of departments
+    } catch (error) {
+      console.error("Error fetching departments:", error?.message);
+    }
+  };
   useEffect(() => {
     setUser(JSON.parse(localStorage?.getItem("user")));
     // Fetch data when the component mounts
+    fetchData();
     fetchUsers();
     fetchMDR();
   }, []);
@@ -169,8 +226,8 @@ export default function Projects() {
         title="Create Permissions"
         width={416}
         centered
-        visible={projectModalVisible}
-        onCancel={projectModalCancel}
+        visible={DocumentPermissionModalVisible}
+        onCancel={DocumentPermissionModalCancel}
         footer={null}
         closeIcon={
           <RiCloseFill className="remix-icon text-color-black-100" size={24} />
@@ -223,54 +280,14 @@ export default function Projects() {
                 block
                 type="primary"
                 htmlType="submit"
-                onClick={() => addProject()}
+                onClick={() => addPermission()}
               >
                 Add
               </Button>
             </Col>
 
             <Col md={12} span={24} className="hp-mt-sm-12 hp-pl-sm-0 hp-pl-12">
-              <Button block onClick={projectModalCancel}>
-                Cancel
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-      </Modal>
-
-      <Modal
-        title="Document Permissions"
-        width={416}
-        centered
-        visible={permissionModalVisible}
-        onCancel={permissionModalCancel}
-        footer={null}
-        closeIcon={
-          <RiCloseFill className="remix-icon text-color-black-100" size={24} />
-        }
-      >
-        <Form layout="vertical" name="basic">
-          <Form.Item label="Project Name" name="projectId">
-            <Select
-              options={projectOptions}
-              value={projectId}
-              onChange={(e) => setProjectId(e)}
-            />
-          </Form.Item>
-
-          <Row>
-            <Col md={12} span={24} className="hp-pr-sm-0 hp-pr-12">
-              <Button
-                block
-                type="primary"
-                htmlType="submit"
-                onClick={() => addProject()}
-              >
-                Add
-              </Button>
-            </Col>
-            <Col md={12} span={24} className="hp-mt-sm-12 hp-pl-sm-0 hp-pl-12">
-              <Button block onClick={projectModalCancel}>
+              <Button block onClick={DocumentPermissionModalCancel}>
                 Cancel
               </Button>
             </Col>
@@ -279,7 +296,7 @@ export default function Projects() {
       </Modal>
 
       <div style={{ textAlign: "right", marginBottom: "16px" }}>
-        <Button type="primary" onClick={projectModalShow}>
+        <Button type="primary" onClick={DocumentPermissionModalShow}>
           Add Permissions
         </Button>
       </div>
